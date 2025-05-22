8c99d86aaea969451aa7bfc5e12110c86c949307
syed daniyal hashmi <syeddaniyalhashmi123@gmail.com>
2025-05-22 14:09:12 +0500

    Add something

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -7,12 +7,14 @@ const nextConfig = {
   webpack: (config: any) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname,'src'),
+      '@': path.resolve(__dirname, 'src'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@components': path.resolve(__dirname, '/components'),
     };
     return config;
   },
 };
-
+export default nextConfig;
 
 // http://seal-app-8m3g5.ondigitalocean.app
 
