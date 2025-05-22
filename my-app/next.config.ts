37dc36877692097e7018c1696b1f42be9febb68c
syed daniyal hashmi <syeddaniyalhashmi123@gmail.com>
2025-05-22 13:53:37 +0500

    Add changes

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
   webpack: (config: any) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': path.resolve(__dirname,'src'),
     };
     return config;
   },
