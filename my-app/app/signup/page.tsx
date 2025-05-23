3aeae770e28d73fdfff20c28c3a757e03f57fd82
syed daniyal hashmi <syeddaniyalhashmi123@gmail.com>
2025-05-24 01:56:00 +0500

    new issue fix

--- my-app/app/signup/page.tsx
+++ my-app/app/signup/page.tsx
@@ -1,5 +1,5 @@
 // /app/signup/page.tsx
-import SignupPage from './client/SignupPage.tsx';
+import SignupPage from './client/SignupPage';
 
 export default function Page() {
   return <SignupPage />;
