9be50041fd0087e470729e9f1e3a73de7fdfddac
syed daniyal hashmi <syeddaniyalhashmi123@gmail.com>
2025-05-23 21:18:10 +0500

    new issue fix

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,7 +3,9 @@
 import { UserProvider } from '@/components/UserContext';
 import './font.css'; // Create this file
 
-export default function RootLayout({ children }) {
+import { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
